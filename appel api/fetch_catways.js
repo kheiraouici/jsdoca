92c7086361250d1a299e5b2b0cf221bbad36f399
kheira <ouicikheira770@gmail.com>
2025-05-09 16:34:40 +0200

    appel api

--- appel api/fetch_catways.js
+++ appel api/fetch_catways.js
@@ -1,6 +1,6 @@
 async function catways (){
    try {
-    let result = await fetch('http://localhost:3000/catways');
+    let result = await fetch('https://localhost:3000/catways');
     let resultJson = await result.json ();
     let catways = resultJson.data;
     let catwayDom = document.getElementById("catway");
