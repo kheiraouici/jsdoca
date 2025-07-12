4e7db4572c4dfdc80bd4a253679ff17c72e15ef7
kheira <ouicikheira770@gmail.com>
2025-07-12 16:35:54 +0200

    structure correction

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,17 @@ var router = express.Router();
 const userRoute = require('../routes/users');
 const catwayRoute = require('../routes/catways');
 const bookingRoute = require('../routes/booking');
+const dashboardRoute = require('../routes/dashboard');
+
+
 
 
 router.use('/users', userRoute);
 router.use('/catways', catwayRoute);
 router.use('/catways', bookingRoute);
+router.use('/tableau-de-bord', dashboardRoute);
+
+
 
 
 module.exports = router;
